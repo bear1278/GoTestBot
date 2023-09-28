04751ac920696e0d80d3ea8c86b60804afde6dfe
bear1278 <grigorijanusik@gmail.com>
2023-09-28 11:42:12 +0300

    Fixed errors

--- pkg/db/MySQL/Mysql.go
+++ pkg/db/MySQL/Mysql.go
@@ -8,104 +8,95 @@ import (
 	tg "github.com/bear1278/GoTestBot/pkg/telegram"
 )
 
-type DbRepository struct{
+type DbRepository struct {
 	db *sql.DB
 }
 
-
-func NewDbRepository(db *sql.DB) *DbRepository{
-	return &DbRepository{db:db}
+func NewDbRepository(db *sql.DB) *DbRepository {
+	return &DbRepository{db: db}
 }
 
-
-
-
-func (d* DbRepository) CheckAlreadyMark(ChatID int64, task string) (bool,error){
-	rows, err:= d.db.Query("select mark from tasks where chatid=? and name=?", ChatID, task)
-	if err!=nil{
-		return false,err
+func (d *DbRepository) CheckAlreadyMark(ChatID int64, task string) (bool, error) {
+	rows, err := d.db.Query("select mark from tasks where chatid=? and name=?", ChatID, task)
+	if err != nil {
+		return false, err
 	}
 	defer rows.Close()
-	mark:=""
-	for rows.Next(){
-		err=rows.Scan(&mark)
-		if err!=nil{
-			return false,err
+	mark := ""
+	for rows.Next() {
+		err = rows.Scan(&mark)
+		if err != nil {
+			return false, err
 		}
 	}
-	if mark=="✅"{
-		return true,nil
-	}else{
-		return false,nil
+	if mark == "✅" {
+		return true, nil
+	} else {
+		return false, nil
 	}
 }
 
-
-func (d* DbRepository) CheckTask(ChatId int64, task string) (bool,error){
-	rows, err:= d.db.Query("select name from tasks where chatid=? and name=?", ChatId, task)
-	if err!=nil{
-		return false,err
+func (d *DbRepository) CheckTask(ChatId int64, task string) (bool, error) {
+	rows, err := d.db.Query("select name from tasks where chatid=? and name=?", ChatId, task)
+	if err != nil {
+		return false, err
 	}
 	defer rows.Close()
-	name:=""
-	for rows.Next(){
-		err=rows.Scan(&name)
-		if err!=nil{
-			return false,err
+	name := ""
+	for rows.Next() {
+		err = rows.Scan(&name)
+		if err != nil {
+			return false, err
+		}
 	}
-	}
-	if name!=""{
-		return true,nil
-	}else{
-		return false,nil
+	if name != "" {
+		return true, nil
+	} else {
+		return false, nil
 	}
 }
 
-func (d * DbRepository) List(ChatID int64) (string, error){
-	rows, err:= d.db.Query("select name, date, mark from tasks where chatid=?", ChatID)
+func (d *DbRepository) List(ChatID int64) (string, error) {
+	rows, err := d.db.Query("select name, date, mark from tasks where chatid=?", ChatID)
 
-	if err!=nil{
-		return "",err
+	if err != nil {
+		return "", err
 	}
 	var date string
 	defer rows.Close()
-	user:=tg.NewUser(ChatID)
-	for rows.Next(){
-		t:=tg.Task{}
+	user := tg.NewUser(ChatID)
+	for rows.Next() {
+		t := tg.Task{}
 
-		err:=rows.Scan(&t.Name,&date,&t.Mark)
-		t.Date,_=time.Parse("2006-01-02", date)
-		
-		if err!=nil{
+		err := rows.Scan(&t.Name, &date, &t.Mark)
+		t.Date, _ = time.Parse("2006-01-02", date)
+
+		if err != nil {
 			log.Print(err)
 			continue
 		}
-		user.SetTask(t.Name,t.Date,t.Mark)
+		user.SetTask(t.Name, t.Date, t.Mark)
 
 	}
 	return user.GetListTask(), nil
-	
+
 }
 
+func (d *DbRepository) Add(ChatID int64, task string) error {
 
-func (d * DbRepository) Add(ChatID int64, task string) error{
-
-
-	_,err:=d.db.Exec("Insert into tasks values (?,?,?,?)", ChatID, task, time.Now().Format("2006-01-02") ,"❗️")
+	_, err := d.db.Exec("Insert into tasks values (?,?,?,?)", ChatID, task, time.Now().Format("2006-01-02"), "❗️")
 
 	return err
 }
 
-
-func (d * DbRepository) Delete(ChatID int64, task string) error{
-	_,err:=d.db.Exec("Delete from tasks where chatid=? and name=?",ChatID,task)
+func (d *DbRepository) Delete(ChatID int64, task string) error {
+	_, err := d.db.Exec("Delete from tasks where chatid=? and name=?", ChatID, task)
 	return err
 
 }
 
+func (d *DbRepository) Mark(ChatID int64, task string) error {
 
-func (d * DbRepository) Mark(ChatID int64, task string) error{
-
-	_,err:=d.db.Exec("Update tasks set mark='✅' where chatid=? and name=?",ChatID,task)
+	_, err := d.db.Exec("Update tasks set mark='✅' where chatid=? and name=?", ChatID, task)
 	return err
 }
--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -6,193 +6,142 @@ import (
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 )
 
-
-func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error{
+func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error {
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.UnvalidCommand)
-	_,err:=b.bot.Send(msg)
-			return err
+	_, err := b.bot.Send(msg)
+	return err
 }
 
-
-func (b *Bot) handleCommand(command *tgbotapi.Message) error{
-	switch command.Command(){
-		case commandStart: 
-			b.handleStartCommand(command)
-		case commandAddNewTask:
-			b.checkUserList(command,"add")
-		case commandDeleteTask:
-			b.checkUserList(command,"delete")
-		case commandMarkTask:
-			b.checkUserList(command,"mark")
-		case commandListTask:
-			b.handleListTaskCommand(command)
-		default: 
-			b.handleUnknownCommand(command)
+func (b *Bot) handleCommand(command *tgbotapi.Message) error {
+	switch command.Command() {
+	case commandStart:
+		return b.handleStartCommand(command)
+	case commandAddNewTask:
+		return b.checkUserList(command, "add")
+	case commandDeleteTask:
+		return b.checkUserList(command, "delete")
+	case commandMarkTask:
+		return b.checkUserList(command, "mark")
+	case commandListTask:
+		return b.handleListTaskCommand(command)
+	default:
+		return b.handleUnknownCommand(command)
 	}
 	return nil
 }
 
-
-
-func (b *Bot) handleStartCommand(message *tgbotapi.Message) error{
+func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.Start)
-	_,err:=b.bot.Send(msg)
+	_, err := b.bot.Send(msg)
 	return err
 }
 
+func (b *Bot) handleAddTaskCommand(message *tgbotapi.Message) error {
 
-func (b *Bot) handleAddTaskCommand(message *tgbotapi.Message) error{
-	// for i,user:=range ListOfUsers{
-	// 	if user.CompareId(message.Chat.ID){
-	// 		user.SetTask(message.Text)
-	// 		ListOfUsers[i]=user
-	// 		break
-	// 	}
-	// }
-	
-	delete(MapOfTasks,message.Chat.ID)
+	delete(MapOfTasks, message.Chat.ID)
 
-	check,err:=b.db.CheckTask(message.Chat.ID, message.Text)
-	if err!=nil{
+	check, err := b.db.CheckTask(message.Chat.ID, message.Text)
+	if err != nil {
 		return nil
 	}
-	if check{
+	if check {
 		return errIndenticalTask
 	}
 
-	err=b.db.Add(message.Chat.ID,message.Text)
-	if err!=nil{
+	err = b.db.Add(message.Chat.ID, message.Text)
+	if err != nil {
 		return err
 	}
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.TaskAdded)
-	_, err=b.bot.Send(msg)
-	
+	_, err = b.bot.Send(msg)
+
 	return err
 }
 
 // исправить  единоообразие методов
-func (b *Bot) handleDeleteTaskCommand(message *tgbotapi.Message) error{
+func (b *Bot) handleDeleteTaskCommand(message *tgbotapi.Message) error {
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.TaskDelete)
-	// for i,user:=range ListOfUsers{
-	// 	if user.CompareId(message.Chat.ID){
-	// 		if !user.DeleteTask(message.Text){
-	// 			msg.Text="Task nt found"
-	// 		}
-	// 		ListOfUsers[i]=user
-	// 		break
-	// 	}
-	// }
-	delete(MapOfTasks,message.Chat.ID)
-	check,err:=b.db.CheckTask(message.Chat.ID, message.Text)
-	if err!=nil{
+
+	delete(MapOfTasks, message.Chat.ID)
+	check, err := b.db.CheckTask(message.Chat.ID, message.Text)
+	if err != nil {
 		return nil
 	}
-	if !check{
+	if !check {
 		return errTaskNotFound
 	}
 
-	err=b.db.Delete(message.Chat.ID,message.Text)
-	if err!=nil{
+	err = b.db.Delete(message.Chat.ID, message.Text)
+	if err != nil {
 		return err
 	}
-	_, err=b.bot.Send(msg)
-	
+	_, err = b.bot.Send(msg)
+
 	return err
 }
 
-func (b *Bot) handleMarkTaskCommand(message *tgbotapi.Message) error{
+func (b *Bot) handleMarkTaskCommand(message *tgbotapi.Message) error {
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.TaskMark)
-	// for i,user:=range ListOfUsers{
-	// 	if user.CompareId(message.Chat.ID){
-	// 		if !user.MarkTask(message.Text){
-	// 			msg.Text="Task not found"
-	// 		}
-	// 		ListOfUsers[i]=user
-	// 		break
-	// 	}
-	// }
-	delete(MapOfTasks,message.Chat.ID)
-	check,err:=b.db.CheckTask(message.Chat.ID, message.Text)
-	if err!=nil{
+
+	delete(MapOfTasks, message.Chat.ID)
+	check, err := b.db.CheckTask(message.Chat.ID, message.Text)
+	if err != nil {
 		return nil
 	}
-	if !check{
+	if !check {
 		return errTaskNotFound
 	}
 
-	check,err=b.db.CheckAlreadyMark(message.Chat.ID, message.Text)
-	if err!=nil{
+	check, err = b.db.CheckAlreadyMark(message.Chat.ID, message.Text)
+	if err != nil {
 		return nil
 	}
-	if check{
+	if check {
 		return errAlreadyMark
 	}
 
-	err=b.db.Mark(message.Chat.ID,message.Text)
+	err = b.db.Mark(message.Chat.ID, message.Text)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
-	
-	_, err=b.bot.Send(msg)
-	
+
+	_, err = b.bot.Send(msg)
+
 	return err
 
 }
 
+func (b *Bot) checkUserList(message *tgbotapi.Message, mode string) error {
 
-func (b *Bot) checkUserList (message *tgbotapi.Message, mode string) error{
-	// if len(ListOfUsers)>0{
-	// 	count:=0
-	// 	for _,v:=range ListOfUsers{
-			
-	// 		if v.CompareId(message.Chat.ID){
-	// 			break
-	// 		}	
-	// 		count ++
-	// 	}
-	// 	if count==len(ListOfUsers) && !ListOfUsers[len(ListOfUsers)-1].CompareId(message.Chat.ID){
-	// 		ListOfUsers=append(ListOfUsers, *NewUser(message.Chat.ID))
-	// 	}
-	// }else{
-	// 	ListOfUsers=append(ListOfUsers, *NewUser(message.Chat.ID))
-	// }
-	MapOfTasks[message.Chat.ID]=mode
+	MapOfTasks[message.Chat.ID] = mode
 	msg := tgbotapi.NewMessage(message.Chat.ID, b.Messages.EnterTask)
-	_,err:=b.bot.Send(msg)
+	_, err := b.bot.Send(msg)
 	return err
 }
 
+func (b *Bot) handleListTaskCommand(message *tgbotapi.Message) error {
 
-func (b *Bot) handleListTaskCommand(message *tgbotapi.Message) error{
-	// user:=NewUser(message.Chat.ID)
-	// for _,i:=range ListOfUsers{
-	// 	if i.CompareId(message.Chat.ID){
-	// 		user=&i
-	// 		break
-	// 	}
-	// }
-	listOfTasks, err:=b.db.List(message.Chat.ID)
+	listOfTasks, err := b.db.List(message.Chat.ID)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
-	if listOfTasks==""{
+	if listOfTasks == "" {
 		return errNoTasks
 	}
 
 	msg := tgbotapi.NewMessage(message.Chat.ID, listOfTasks)
-	_,err=b.bot.Send(msg)
+	_, err = b.bot.Send(msg)
 	return err
 }
 
-
-func (b *Bot) handleMessege(message *tgbotapi.Message) error{
+func (b *Bot) handleMessege(message *tgbotapi.Message) error {
 	log.Printf("[%s] %s", message.From.UserName, message.Text)
-	task,ok:=MapOfTasks[message.Chat.ID]
-	if ok{
-		switch task{
+	task, ok := MapOfTasks[message.Chat.ID]
+	if ok {
+		switch task {
 		case "add":
 			return b.handleAddTaskCommand(message)
 		case "delete":
@@ -200,8 +149,8 @@ func (b *Bot) handleMessege(message *tgbotapi.Message) error{
 		case "mark":
 			return b.handleMarkTaskCommand(message)
 		}
-		}else{
-			return errUnvalidText
-		}
-		return nil
+	} else {
+		return errUnvalidText
 	}
+	return nil
+}
